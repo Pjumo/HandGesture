6c337d551ec95657dd044683d51061e55993dcde
Pjumo <parkjunmoa@naver.com>
2024-04-10 23:30:41 +0900

    [Add] Camera Mp4 파일에서  데이터 추출-1

--- gather_dataset_cam.py
+++ gather_dataset_cam.py
@@ -16,21 +16,35 @@ fps = 30
 class WebCamData:
     def __init__(self, video_path):
         self.video_path = video_path
+        if video_path.find('click'):
+            self.action = 'click'
+            self.action_label = 1
+        else:
+            self.action = 'doubleclick'
+            self.action_label = 2
 
     def split_video(self):
         cv2.destroyAllWindows()
         cap = cv2.VideoCapture(self.video_path)
+        os.makedirs(f'dataset/{self.action}', exist_ok=True)
         if cap.isOpened():
             cnt = 0
             current_dist = 0
             gesture_cnt = 0
-            data = []
 
             ret, img = cap.read()
             cv2.imshow('webcam', img)
 
+            data = []
+            action_data = []
+
             while True:
-                cnt += 1
+                if cnt > 0:
+                    cnt += 1
+                elif cnt == 51:
+                    cnt = 0
+                    # action_data -> numpy 파일화 코드 추가 필요
+                    action_data = []
                 ret, img = cap.read()
                 if not ret:
                     break
@@ -45,14 +59,27 @@ class WebCamData:
                         for j, lm in enumerate(res.landmark):
                             joint[j] = [lm.x, lm.y, lm.z]
 
+                        d = np.concatenate([joint, [0, 0, 0]])
+                        if cnt > 0:
+                            action_data.append(d)
+                        else:
+                            data.append(d)
+
                         dist = np.linalg.norm(joint[8] - joint[4])
                         if current_dist == 0:
                             print('start')
                         elif current_dist > 0.1 >= dist:
+                            print(f'down{gesture_cnt}')
                             gesture_cnt += 1
-                            print(f'down{gesture_cnt}, {cnt}')
+                            # action 라벨링 오류 날수도
+                            action_data.append(np.concatenate(
+                                [data[-5:][:-2], [self.action_label, self.action_label, self.action_label]]))
+                            # data 안에 들어 있는 50 프레임 nothing numpy 파일화 필요
+                            data = []
+                            cnt = 5
                         elif current_dist <= 0.1 < dist:
-                            print(f'up{gesture_cnt}, {cnt}')
+                            print(f'up{gesture_cnt}')
+
                         current_dist = dist
                         mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
 
