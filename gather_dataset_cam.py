864afce7e7875aed90f6b69427e20d1915622b92
Pjumo <parkjunmoa@naver.com>
2024-04-30 00:13:09 +0900

    [ADD] gathering dataset from video

--- gather_dataset_cam.py
+++ gather_dataset_cam.py
@@ -16,35 +16,64 @@ fps = 30
 class WebCamData:
     def __init__(self, video_path):
         self.video_path = video_path
-        if video_path.find('click'):
-            self.action = 'click'
-            self.action_label = 1
-        else:
+        if video_path.find('doubleclick'):  # 동작 라벨링
             self.action = 'doubleclick'
             self.action_label = 2
+        else:
+            self.action = 'click'
+            self.action_label = 1
+
+    def record_index(self, action_count, nothing_count):
+        # 마지막 index 기록
+        f_w = open(f'dataset/{self.action}/last_idx.txt', 'w')
+        f_w.write(str(action_count))
+        f_w.close()
+        f_w = open(f'dataset/nothing/last_idx.txt', 'w')
+        f_w.write(str(nothing_count))
+        f_w.close()
 
     def split_video(self):
         cv2.destroyAllWindows()
-        cap = cv2.VideoCapture(self.video_path)
+        cap = cv2.VideoCapture(self.video_path)  # opencv로 녹화 영상 틀기
         os.makedirs(f'dataset/{self.action}', exist_ok=True)
+        os.makedirs('dataset/nothing', exist_ok=True)
+
+        action_count = 0
+        if os.path.isfile(f'dataset/{self.action}/last_idx.txt'):
+            f_r = open(f'dataset/{self.action}/last_idx.txt', 'r')
+            action_count = int(f_r.readline())
+            f_r.close()
+
+        nothing_count = 0
+        if os.path.isfile(f'dataset/nothing/last_idx.txt'):
+            f_r = open(f'dataset/nothing/last_idx.txt', 'r')
+            nothing_count = int(f_r.readline())
+            f_r.close()
+
         if cap.isOpened():
-            cnt = 0
-            current_dist = 0
-            gesture_cnt = 0
+            cnt = 0  # 0보다 클 때 데이터 수집중
+            current_dist = 0  # 최근 엄지와 검지 사이의 거리
+            doubleclick_det = 0  # doubleclick의 경우 검지와 엄지 사이의 거리가 두번 왔다 갔다함
 
             ret, img = cap.read()
             cv2.imshow('webcam', img)
 
-            data = []
-            action_data = []
+            data = []  # 기본 데이터 배열
+            action_data = []  # 제스처 데이터 배열
 
+            gesture_cnt = 0
             while True:
-                if cnt > 0:
-                    cnt += 1
-                elif cnt == 51:
+                if cnt == 50:  # 제스처 데이터 수집 완료
+                    gesture_cnt += 1
                     cnt = 0
-                    # action_data -> numpy 파일화 코드 추가 필요
+                    np.save(os.path.join(f'dataset/{self.action}', f'seq_{action_count}'), action_data)
                     action_data = []
+                    doubleclick_det = 0
+                    if gesture_cnt == 8:
+                        self.record_index(action_count, nothing_count)
+                        break
+                elif cnt > 0:  # 제스처 데이터 수집중
+                    cnt += 1
                 ret, img = cap.read()
                 if not ret:
                     break
@@ -59,26 +88,32 @@ class WebCamData:
                         for j, lm in enumerate(res.landmark):
                             joint[j] = [lm.x, lm.y, lm.z]
 
-                        d = np.concatenate([joint, [0, 0, 0]])
+                        d = np.append(joint.flatten(), 0)
                         if cnt > 0:
-                            action_data.append(d)
+                            action_data = np.append(action_data, d.reshape(1, 64), axis=0)
                         else:
                             data.append(d)
 
                         dist = np.linalg.norm(joint[8] - joint[4])
-                        if current_dist == 0:
-                            print('start')
-                        elif current_dist > 0.1 >= dist:
-                            print(f'down{gesture_cnt}')
-                            gesture_cnt += 1
-                            # action 라벨링 오류 날수도
-                            action_data.append(np.concatenate(
-                                [data[-5:][:-2], [self.action_label, self.action_label, self.action_label]]))
-                            # data 안에 들어 있는 50 프레임 nothing numpy 파일화 필요
+                        if current_dist - dist > 0.03 and doubleclick_det == 0:
+                            action_count += 1
+                            doubleclick_det = 1
+                            print(f'{self.action}_{action_count}')
+                            data = np.array(data)
+
+                            # 동작의 sequence를 -5프레임부터 시작
+                            action_data = data[-5:, :-1]
+                            labels = np.reshape([self.action_label] * 5, (5, 1))
+                            action_data = np.concatenate([action_data, labels], axis=1)  # 라벨 붙이기
+                            action_data = np.reshape(action_data, (5, 64))
+
+                            # nothing data 분류
+                            if data.size >= 50 * 64:
+                                nothing_count += 1
+                                nothing_data = data[:50, :]
+                                np.save(os.path.join('dataset/nothing', f'seq_{nothing_count}'), nothing_data)
                             data = []
                             cnt = 5
-                        elif current_dist <= 0.1 < dist:
-                            print(f'up{gesture_cnt}')
 
                         current_dist = dist
                         mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import gather_dataset_cam as cam
 
-path = './video_webcam/click_10.mp4'
-webcam = cam.WebCamData(path)
-webcam.show_skeleton()
+for i in range(20):
+    path = f'./video_webcam/doubleclick_{i+1}.mp4'
+    webcam = cam.WebCamData(path)
+    print(i+1)
+    webcam.split_video()
